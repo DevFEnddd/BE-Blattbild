3e13ee54a6abc257bdffe6f6faef43bb7e429c97
kiubaka <nguyenminhhoang.utt@gmail.com>
2024-08-15 18:51:48 +0700

    hoang

--- src/controllers/form.controller.js
+++ src/controllers/form.controller.js
@@ -3,35 +3,35 @@ import FormServices from '../services/form.service.js'
 
 const createForm = async (req, res, next) => {
     try {
-        const { name,
+        const { name,lname,fname,communicate,notify,
           email,
           phone,
           topic } = req.body
 
-          if (!name) {
-            return res.status(500).json({
-                status: 'ERR',
-                message: 'Validation Name!'
-            })
-          }
-          if (!email) {
-            return res.status(500).json({
-                status: 'ERR',
-                message: 'Validation Email!'
-            })
-          }
-          if (!phone) {
-            return res.status(500).json({
-                status: 'ERR',
-                message: 'Validation Phone!'
-            })
-          }
-          if (!topic) {
-            return res.status(500).json({
-                status: 'ERR',
-                message: 'Validation Topic!'
-            })
-          }
+          // if (!name) {
+          //   return res.status(500).json({
+          //       status: 'ERR',
+          //       message: 'Validation Name!'
+          //   })
+          // }
+          // if (!email) {
+          //   return res.status(500).json({
+          //       status: 'ERR',
+          //       message: 'Validation Email!'
+          //   })
+          // }
+          // if (!phone) {
+          //   return res.status(500).json({
+          //       status: 'ERR',
+          //       message: 'Validation Phone!'
+          //   })
+          // }
+          // if (!topic) {
+          //   return res.status(500).json({
+          //       status: 'ERR',
+          //       message: 'Validation Topic!'
+          //   })
+          // }
         const response = await FormServices.createForm(req)
         return res.status(200).json(response)
     } catch(err) {
